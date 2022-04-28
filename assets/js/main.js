40604b8dfc311944a8464c2ed77a46ea66d10134
Oscar <oscargonz150@gmail.com>
2022-04-28 13:07:24 -0600

    details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-//based on https://dribbble.com/shots/3913847-404-page
 
 var pageX = $(document).width();
 var pageY = $(document).height();
@@ -6,15 +5,13 @@ var mouseY=0;
 var mouseX=0;
 
 $(document).mousemove(function( event ) {
-  //verticalAxis
+  
   mouseY = event.pageY;
   yAxis = (pageY/2-mouseY)/pageY*300; 
-  //horizontalAxis
+
   mouseX = event.pageX / -pageX;
   xAxis = -mouseX * 100 - 100;
 
   $('.box__ghost-eyes').css({ 'transform': 'translate('+ xAxis +'%,-'+ yAxis +'%)' }); 
-
-  //console.log('X: ' + xAxis);
 
 });
